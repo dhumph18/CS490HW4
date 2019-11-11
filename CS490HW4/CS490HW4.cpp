814e550f691a9550619a56a57c028eef95ee535e
Drew Humphrey <dmh0032@uah.edu>
2019-11-11 09:39:24 -0600

    Only missing documentation

--- CS490HW4/CS490HW4.cpp
+++ CS490HW4/CS490HW4.cpp
@@ -1,8 +1,6 @@
 // CS490HW4.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 //TODO List
-// - Implement second quantum size
-// - Round data to 2 decimal places
 // - Document/Comment code
 // - One page summary
 
@@ -23,15 +21,16 @@ struct Process
 	int cpuTime;
 	int completionTime; //Time that the process completed.
 	int tat; //Turn around time
-	double normTat; //Normalized turn around time
+	float normTat; //Normalized turn around time
 	int rotations; //The number of times that the process had to be recycled into the queue
 };
 
 void calculateIndStats(vector<Process> &finishedList);
 void readInputFile(vector<int> &quantumTimes, vector<int> &serviceTimes, queue<Process> &processes);
-int simulateRoundRobin(queue<Process> &processQueue, int quantum, vector<Process> *finishedList);
+int simulateRoundRobin(queue<Process> processQueue, int quantum, vector<Process> *finishedList);
 void printIndStats(vector<Process> &finishedList, int quantum, bool firstOutput);
 void printSystemStats(vector<Process>& finishedList, int quantum, int finalClock);
+float rounding(float num);
 
 int main()
 {
@@ -43,19 +42,21 @@ int main()
 
 	readInputFile(inputQuantumTimes, inputServiceTimes, processQueue);
 
+	cout << "Creating output file named outputHWK4.txt....." << endl;
+
 	//Need to later run this twice for each quantum size
-	//for (int i = 0; i < (int)inputQuantumTimes.size(); i++) {
-	vector<Process>* finishedList = new vector<Process>();
+	for (int i = 0; i < (int)inputQuantumTimes.size(); i++) {
+		vector<Process>* finishedList = new vector<Process>();
 
-	
-	//change to use i for quantum sizes
-	int finalClock = simulateRoundRobin(processQueue, inputQuantumTimes.at(0), finishedList);
+		int finalClock = simulateRoundRobin(processQueue, inputQuantumTimes.at(i), finishedList);
 
-	calculateIndStats(*finishedList);
-	printIndStats(*finishedList, inputQuantumTimes.at(0), firstOutput); //change to i
-	printSystemStats(*finishedList, inputQuantumTimes.at(0), finalClock); //change to i
-	firstOutput = false;
-	//}
+		calculateIndStats(*finishedList);
+		printIndStats(*finishedList, inputQuantumTimes.at(i), firstOutput); //change to i
+		printSystemStats(*finishedList, inputQuantumTimes.at(i), finalClock); //change to i
+		firstOutput = false;
+	}
+
+	cout << "\nFinished" << endl;
 }
 
 //Read in the process service times from the file
@@ -71,7 +72,6 @@ void readInputFile(vector<int> &quantumTimes, vector<int> &serviceTimes, queue<P
 	ss2 << quantums;
 	while (ss2 >> num) {
 		quantumTimes.push_back(num);
-		cout << num << endl;
 	}
 
 	while (!data.eof()) {
@@ -87,12 +87,11 @@ void readInputFile(vector<int> &quantumTimes, vector<int> &serviceTimes, queue<P
 
 	for (int i = 0; i < (int)serviceTimes.size(); i++) {
 		struct Process p { id++, 0, serviceTimes.at(i), 0 };
-		cout << "Process #" << p.processId << " Service Time = " << p.serviceTime << endl;
 		processQueue.push(p);
 	}
 }
 
-int simulateRoundRobin(queue<Process>& processQueue, int quantum, vector<Process>* finishedList) {
+int simulateRoundRobin(queue<Process> processQueue, int quantum, vector<Process>* finishedList) {
 	int clock = 0;
 
 	while (!processQueue.empty()) {
@@ -128,9 +127,8 @@ void calculateIndStats(vector<Process> &finishedList) {
 	for (int i = 0; i < (int)finishedList.size(); i++) {
 		Process p = finishedList.at(i);
 		p.tat = p.completionTime - p.arrivalTime;
-		p.normTat = p.tat / (double)p.serviceTime;
+		p.normTat = p.tat / (float)p.serviceTime;
 		finishedList.at(i) = p;
-		//cout << p.tat << endl;
 	}
 }
 
@@ -148,7 +146,7 @@ void printIndStats(vector<Process>& finishedList, int quantum, bool firstOutput)
 
 	for (int i = 0; i < 15; i++) {
 		Process p = finishedList.at(i);
-		output << setw(5) << p.processId << setw(15) << p.arrivalTime << setw(15) << p.serviceTime << setw(20) << p.completionTime << setw(10) << p.tat << setw(13) << p.normTat << endl;
+		output << setw(5) << p.processId << setw(15) << p.arrivalTime << setw(15) << p.serviceTime << setw(20) << p.completionTime << setw(10) << p.tat << setw(13) << rounding(p.normTat) << endl;
 
 	}
 
@@ -164,10 +162,10 @@ void printSystemStats(vector<Process>& finishedList, int quantum, int finalClock
 	int medTatSum = 0;
 	int longTatSum = 0;
 	int systemTatSum = 0;
-	double shortNTatSum = 0;
-	double medNTatSum = 0;
-	double longNTatSum = 0;
-	double systemNTatSum = 0;
+	float shortNTatSum = 0;
+	float medNTatSum = 0;
+	float longNTatSum = 0;
+	float systemNTatSum = 0;
 
 	for (int i = 0; i < (int)finishedList.size(); i++) {
 		Process p = finishedList.at(i);
@@ -190,15 +188,15 @@ void printSystemStats(vector<Process>& finishedList, int quantum, int finalClock
 		systemNTatSum += p.normTat;
 	}
 
-	double shortTatAvg = shortTatSum / (double)shortSum;
-	double medTatAvg = medTatSum / (double)medSum;
-	double longTatAvg = longTatSum / (double)longSum;
-	double systemTatAvg = systemTatSum / (double)finishedList.size();
+	float shortTatAvg = shortTatSum / (float)shortSum;
+	float medTatAvg = medTatSum / (float)medSum;
+	float longTatAvg = longTatSum / (float)longSum;
+	float systemTatAvg = systemTatSum / (float)finishedList.size();
 
-	double shortNTatAvg = shortNTatSum / (double)shortSum;
-	double medNTatAvg = medNTatSum / (double)medSum;
-	double longNTatAvg = longNTatSum / (double)longSum;
-	double systemNTatAvg = systemNTatSum / (double)finishedList.size();
+	float shortNTatAvg = shortNTatSum / (float)shortSum;
+	float medNTatAvg = medNTatSum / (float)medSum;
+	float longNTatAvg = longNTatSum / (float)longSum;
+	float systemNTatAvg = systemNTatSum / (float)finishedList.size();
 
 	ofstream output;
 	output.open("HWK4output.txt", ios_base::app);
@@ -207,10 +205,15 @@ void printSystemStats(vector<Process>& finishedList, int quantum, int finalClock
 	output << "Quantum = " << quantum << ", Number of clock ticks = " << finalClock << endl;
 	output << setw(30) << "Short" << setw(15) << "Medium" << setw(15) << "Long" << setw(15) << "System" << endl;
 	output << "Number of processes " << setw(10) << shortSum << setw(15) << medSum << setw(15) << longSum << setw(15) << finishedList.size() << endl;
-	output << "Average TAT    " << setw(15) << shortTatAvg << setw(15) << medTatAvg << setw(15) << longTatAvg << setw(15) << systemTatAvg << endl;
-	output << "Average NTAT   " << setw(15) << shortNTatAvg << setw(15) << medNTatAvg << setw(15) << longNTatAvg << setw(15) << systemNTatAvg << endl;
+	output << "Average TAT    " << setw(15) << rounding(shortTatAvg) << setw(15) << rounding(medTatAvg) << setw(15) << rounding(longTatAvg) << setw(15) << rounding(systemTatAvg) << endl;
+	output << "Average NTAT   " << setw(15) << rounding(shortNTatAvg) << setw(15) << rounding(medNTatAvg) << setw(15) << rounding(longNTatAvg) << setw(15) << rounding(systemNTatAvg) << endl;
 	output << "\n" << endl;
 	output.close();
+}
+
+float rounding(float num) {
+	float value = (int)(num * 100 + .5);
+	return (float)value / 100;
 }
 
 // Tips for Getting Started: 
