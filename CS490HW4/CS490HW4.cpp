c763f2835b51e269a004f9a4e5c7ec9bbec26946
Drew Humphrey <dmh0032@uah.edu>
2019-11-10 21:22:46 -0600

    Calculate TATs

--- CS490HW4/CS490HW4.cpp
+++ CS490HW4/CS490HW4.cpp
@@ -4,10 +4,10 @@
 #include <iostream>
 #include <fstream>
 #include <queue>
-//#include <deque>
 #include <vector>
 #include <sstream>
 
+using namespace std;
 
 struct Process
 {
@@ -15,76 +15,90 @@ struct Process
 	int arrivalTime;
 	int serviceTime;
 	int cpuTime;
+	int completionTime; //Time that the process completed.
+	int tat; //Turn around time
+	double normTat; //Normalized turn around time
+	int rotations; //The number of times that the process had to be recycled into the queue
+	string length; //Short (1), Medium (2), Long (>2) corresponding to the number of rotations
 };
 
+void calculateIndStats(vector<Process> &finishedList);
 //void calculateNormTat(vector<Process> &q);
-
-using namespace std;
+void readInputFile(vector<int> &quantumTimes, vector<int> &serviceTimes, queue<Process> &processes);
+void simulateRoundRobin(queue<Process> &processQueue, int quantum, vector<Process> *finishedList);
 
 int main()
 {
 	void calculateTat(vector<Process> &p);
-	vector<int>* quantumTimes = new vector<int>();
 	vector<int> inputServiceTimes;
 	vector<int> inputQuantumTimes;
+	queue<Process> processQueue;
 
+	readInputFile(inputQuantumTimes, inputServiceTimes, processQueue);
 
-	fstream data("HWK4input.txt", ios_base::in);
+	vector<Process>* finishedList = new vector<Process>();
 
-	//int time;
-	//char test[] = "5 7";
-	//stringstream ss;
-	//ss << test;
-	//while (ss >> time) {
-	//	inputTimes.push_back(time);
+	for (int i = 0; i < (int)inputQuantumTimes.size(); i++) {
+		simulateRoundRobin(processQueue, inputQuantumTimes.at(i), finishedList);
+	}
+
+	//for (int i = 0; i < finishedList->size(); i++) {
+	//	cout << "Finished # " << finishedList->at(i).processId << endl;
 	//}
-
-	char* quantums = new char(10);
 	
+	vector<Process>* sortedVec = new vector<Process>();
+	Process init;
+	init.processId = -1;
+	//Initialize the sorted vector of processes to processes
+	for (int i = 0; i < (int)finishedList->size(); i++) {
+		sortedVec->push_back(init);
+	}
+
+	for (int i = 0; i < (int)finishedList->size(); i++) {
+		sortedVec->at(finishedList->at(i).processId) = finishedList->at(i);
+	}
+
+	calculateIndStats(*sortedVec);
+
+}
+
+//Read in the process service times from the file
+//and add the process to the end of the queue
+void readInputFile(vector<int> &quantumTimes, vector<int> &serviceTimes, queue<Process> &processQueue) {
+	int id = 0;
+	fstream data("HWK4input.txt", ios_base::in);
+	char* quantums = new char(10);
 	
 	data.getline(quantums, 10);
 	int num;
 	stringstream ss2;
 	ss2 << quantums;
 	while (ss2 >> num) {
-		inputQuantumTimes.push_back(num);
+		quantumTimes.push_back(num);
 		cout << num << endl;
 	}
-	
 
 	while (!data.eof()) {
-		char* serviceTimes = new char(5);
-		data.getline(serviceTimes, 5);
+		char* inputTime = new char(5);
+		data.getline(inputTime, 5);
 		int time;
 		stringstream ss;
-		ss << serviceTimes;
+		ss << inputTime;
 		while (ss >> time) {
-			inputServiceTimes.push_back(time);
+			serviceTimes.push_back(time);
 		}
 	}
 
-	
-
-	/*cout << "Input data:" << endl;
-	for (int i = 0; i < (int)inputServiceTimes.size(); i++) {
-		cout << inputServiceTimes.at(i) << " ";
-	}
-	cout << endl;*/
-
-	int id = 0;
-	int clock = 0;
-	int quantum = 7; //Change to be able to take in input from file
-	queue<Process> processQueue;
-	vector<Process>* finishedList = new vector<Process>();
-
-	//Read in the process service times from the file
-	//and add the process to the end of the queue
-	for (int i = 0; i < (int)inputServiceTimes.size(); i++) {
+	for (int i = 0; i < (int)serviceTimes.size(); i++) {
 		//Arrival time is??
-		struct Process p { id++, 0, inputServiceTimes.at(i), 0 };
+		struct Process p { id++, 0, serviceTimes.at(i), 0 };
 		cout << "Process #" << p.processId << " Service Time = " << p.serviceTime << endl;
 		processQueue.push(p);
 	}
+}
+
+void simulateRoundRobin(queue<Process>& processQueue, int quantum, vector<Process>* finishedList) {
+	int clock = 0;
 
 	while (!processQueue.empty()) {
 		Process p = processQueue.front();
@@ -103,40 +117,24 @@ int main()
 		clock += neededQuantum;
 
 		if (p.cpuTime == p.serviceTime) {
+			p.completionTime = clock;
 			finishedList->push_back(p);
 		}
 		else {
 			processQueue.push(p);
 		}
 	}
-
-	vector<Process>* sortedVec = new vector<Process>();
-	for (int i = 0; i < (int)finishedList->size(); i++) {
-		sortedVec->at(finishedList->at(i).processId - 1) = finishedList->at(i);
-	}
-
-	calculateTat(*sortedVec);
-	//calculateNormTat(*finishedList);
-
 }
 
-void calculateTat(vector<Process> &sortedVec) {
-	vector<int> tats;
-	for (int i = 0; i < (int)sortedVec.size(); i++) {
-		Process p = sortedVec.at(i);
-		tats.at(i) = p.cpuTime - p.arrivalTime;
-		cout << tats.at(i) << endl;
+void calculateIndStats(vector<Process> &finishedList) {
+	for (int i = 0; i < (int)finishedList.size(); i++) {
+		Process p = finishedList.at(i);
+		p.tat = p.completionTime - p.arrivalTime;
+		p.normTat = p.tat / (double)p.serviceTime;
+		//cout << p.tat << endl;
 	}
 }
 
-//void calculateNormTat(vector<Process> &finishedQueue) {
-//
-//}
-
-
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
 // Tips for Getting Started: 
 //   1. Use the Solution Explorer window to add/manage files
